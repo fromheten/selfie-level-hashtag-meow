fd3b30fa7f942ba9f5165033afecd0821976d155
Martin Josefsson <josefsson.martin@gmail.com>
2014-05-12 17:36:42 +0900

    CameraController takes a photo and opens an ImageEditingController

--- app/controllers/camera_controller.rb
+++ app/controllers/camera_controller.rb
@@ -6,8 +6,14 @@ class CameraController < UIViewController
     self.title = "Camera Controller"
 
     BW::Device.camera.rear.picture(media_types: [:image]) do |result|
-      image_view = UIImageView.alloc.initWithImage(result[:original_image])
-      self.view.addSubview(image_view)
+      # On the navigationController, push an ImageEditingController instance with result
+      image = result[:original_image]
+
+      # image_view = UIImageView.alloc.initWithImage(image)
+      # self.view.addSubview(image_view)
+
+      image_editing_viewcontroller = ImageEditingController.alloc.initWithImage(image)
+      self.navigationController.pushViewController(image_editing_viewcontroller, animated:true)
     end
   end
 end

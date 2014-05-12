5fc60a5dcea232ea22c02e36b62bf619d4f803aa
Martin Josefsson <josefsson.martin@gmail.com>
2014-05-12 15:15:40 +0900

    Shows the image after it's snapped

--- app/controllers/camera_controller.rb
+++ app/controllers/camera_controller.rb
@@ -6,6 +6,8 @@ class CameraController < UIViewController
     self.title = "Camera Controller"
 
     BW::Device.camera.rear.picture(media_types: [:image]) do |result|
+      image_view = UIImageView.alloc.initWithImage(result[:original_image])
+      self.view.addSubview(image_view)
     end
   end
 end

f31b9923c67ec38e38a89e566b23bcd6c27104ef
Martin Josefsson <josefsson.martin@gmail.com>
2014-05-24 15:37:18 +0900

    Check if there is a camera, for debugging

--- app/controllers/camera_controller.rb
+++ app/controllers/camera_controller.rb
@@ -5,15 +5,19 @@ class CameraController < UIViewController
     self.view.backgroundColor = UIColor.whiteColor
     self.title = "Camera Controller"
 
-    BW::Device.camera.rear.picture(media_types: [:image]) do |result|
-      # On the navigationController, push an ImageEditingController instance with result
-      image = result[:original_image]
+    if(BW::Device.camera.rear?)
+      BW::Device.camera.rear.picture(media_types: [:image]) do |result|
+        # On the navigationController, push an ImageEditingController instance with result
+        image = result[:original_image]
 
-      # image_view = UIImageView.alloc.initWithImage(image)
-      # self.view.addSubview(image_view)
-
-      image_editing_viewcontroller = ImageEditingController.alloc.initWithImage(image)
-      self.navigationController.pushViewController(image_editing_viewcontroller, animated:true)
+        # image_view = UIImageView.alloc.initWithImage(image)
+        # self.view.addSubview(image_view)
+        NSLog("image.class: ", image.class)
+        image_editing_viewcontroller = ImageEditingController.alloc.initWithImage(image)
+        self.navigationController.pushViewController(image_editing_viewcontroller, animated:true)
+      end
+    else
+      puts "no camera yo"
     end
   end
 end
